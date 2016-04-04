e6605f73bd05edb66aca7d3ce1a7475c04e63aa6
Amy Jennings <amyjenningsmn@gmail.com>
2016-04-04 16:12:17 -0500

    initial commit, not quite done

---
+++ app.js
@@ -0,0 +1,5 @@
+// var random = require('./random.js');
+// var usd = require('./usd.js');
+var balance = require('./balance.js');
+
+ console.log(random(100,1000000));
---
+++ balance.js
@@ -0,0 +1,6 @@
+var randomThing = require("./random.js");
+var usdThing = require("./usd.js");
+
+console.log(money());
+console.log(randomNumber());
+// This is where we left off
